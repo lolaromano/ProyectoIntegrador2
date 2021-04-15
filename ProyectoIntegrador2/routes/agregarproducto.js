256948d2db83dd8013feef21011be7801a545963
luisanazar <nazaranchorenal@udesa.edu.ar>
2021-04-14 23:14:31 -0300

    empezamos a ordenar el index, cambie los controllers y las rutas

--- ProyectoIntegrador2/routes/agregarproducto.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-let controller = require ("../controllers/agregarproductoController")
-
-router.get('/', controller.index);
-
-
-module.exports = router;
--- ProyectoIntegrador2/routes/index.js
+++ ProyectoIntegrador2/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+let homeController = require ('../controllers/homeController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get ('/', homeController.index);
+
+router.get ('/register', homeController.register);
+
+router.get ('/login', homeController.login);
+
+router.get ('/product', homeController.product);
+
+router.get ('/headerLogueado', homeController.headerLogueado);
+
+router.get ('/searchResults', homeController.searchResults);
+
+router.get ('/productAdd', homeController.productAdd);
+
+router.get ('/profileEdit', homeController.profileEdit);
+
+
+
+
 
 module.exports = router;
