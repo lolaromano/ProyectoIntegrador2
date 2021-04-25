5c6ff9ae8e63cf72c2ce462430106838c007ec2c
luisanazar <nazaranchorenal@udesa.edu.ar>
2021-04-24 22:36:42 -0300

    fotos de usuarios

--- ProyectoIntegrador2/data/users.js
+++ ProyectoIntegrador2/data/users.js
@@ -54,92 +54,213 @@ let users = {
                 id: 5,
                 nombre: "M.",
                 apellido: "Campitelli",
-                imagenUsuario: "/images/users/imagenUsuario5.png"
+                imagenUsuario: "/images/users/imagenUsuario0.png"
             },
             {
                 id: 6,
                 nombre: "D.",
                 apellido: "Cardona",
-                imagenUsuario: "/images/users/imagenUsuario6.png"
+                imagenUsuario: "/images/users/imagenUsuario1.png"
             },
             {
                 id: 7,
                 nombre: "T.",
                 apellido: "Del Pino",
-                imagenUsuario: "/images/users/imagenUsuario7.png"
+                imagenUsuario: "/images/users/imagenUsuario2.png"
             },
             {
                 id: 8,
                 nombre: "G.",
                 apellido: "Corti",
-                imagenUsuario: "/images/users/imagenUsuario8.png"
+                imagenUsuario: "/images/users/imagenUsuario3.png"
             },
             {
                 id: 9,
                 nombre: "C.",
                 apellido: "Ortiz Quirno",
-                imagenUsuario: "/images/users/imagenUsuario9.png"
+                imagenUsuario: "/images/users/imagenUsuario4.png"
             },
             {
                 id: 10,
                 nombre: "B.",
                 apellido: "Gimenez",
-                imagenUsuario: "/images/users/imagenUsuario10.png"
+                imagenUsuario: "/images/users/imagenUsuario0.png"
             },
             {
                 id: 11,
                 nombre: "J.",
                 apellido: "Lipson",
-                imagenUsuario: "/images/users/imagenUsuario11.png"
+                imagenUsuario: "/images/users/imagenUsuario1.png"
             },
             {
                 id: 12,
                 nombre: "L.",
                 apellido: "Romano",
-                imagenUsuario: "/images/users/imagenUsuario12.png"
+                imagenUsuario: "/images/users/imagenUsuario2.png"
             },
             {
                 id: 13,
                 nombre: "L.",
                 apellido: "Nazar Anchorena",
-                imagenUsuario: "/images/users/imagenUsuario13.png"
+                imagenUsuario: "/images/users/imagenUsuario3.png"
             },
             {
                 id: 14,
                 nombre: "R.",
                 apellido: "Castelo",
-                imagenUsuario: "/images/users/imagenUsuario14.png"
+                imagenUsuario: "/images/users/imagenUsuario4.png"
             },
             {
                 id: 15,
                 nombre: "J.P.",
                 apellido: "Ascani",
-                imagenUsuario: "/images/users/imagenUsuario15.png"
+                imagenUsuario: "/images/users/imagenUsuario0.png"
             },
             {
                 id: 16,
                 nombre: "S.",
                 apellido: "De Pellegrin",
-                imagenUsuario: "/images/users/imagenUsuario16.png"
+                imagenUsuario: "/images/users/imagenUsuario1.png"
             },
             {
                 id: 17,
                 nombre: "J.",
                 apellido: "Sterinzon",
-                imagenUsuario: "/images/users/imagenUsuario17.png"
+                imagenUsuario: "/images/users/imagenUsuario2.png"
             },
             {
                 id: 18,
                 nombre: "P.",
                 apellido: "Correa",
-                imagenUsuario: "/images/users/imagenUsuario18.png"
+                imagenUsuario: "/images/users/imagenUsuario3.png"
             },
             {
                 id: 19,
                 nombre: "T.",
                 apellido: "Mihailovich",
-                imagenUsuario: "/images/users/imagenUsuario19.png"
+                imagenUsuario: "/images/users/imagenUsuario4.png"
             },
+            {
+                id: 20,
+                nombre: "J.",
+                apellido: "Perez",
+                imagenUsuario: "/images/users/imagenUsuario0.png"
+            },
+            {
+                id: 21,
+                nombre: "P.",
+                apellido: "Kaan",
+                imagenUsuario: "/images/users/imagenUsuario1.png"
+            },
+            {
+                id: 22,
+                nombre: "T.",
+                apellido: "Sanchez",
+                imagenUsuario: "/images/users/imagenUsuario2.png"
+            },
+            {
+                id: 23,
+                nombre: "L.",
+                apellido: "Gonzalez",
+                imagenUsuario: "/images/users/imagenUsuario3.png"
+            },
+            {
+                id: 24,
+                nombre: "C.",
+                apellido: "Ferroni",
+                imagenUsuario: "/images/users/imagenUsuario4.png"
+            },
+            {
+                id: 25,
+                nombre: "J.",
+                apellido: "Brancateli",
+                imagenUsuario: "/images/users/imagenUsuario0.png"
+            },
+            {
+                id: 26,
+                nombre: "M.",
+                apellido: "Cerro",
+                imagenUsuario: "/images/users/imagenUsuario1.png"
+            },
+            {
+                id: 27,
+                nombre: "I.",
+                apellido: "Rojas",
+                imagenUsuario: "/images/users/imagenUsuario2.png"
+            },
+            {
+                id: 28,
+                nombre: "L.",
+                apellido: "Patiño",
+                imagenUsuario: "/images/users/imagenUsuario3.png"
+            },
+            {
+                id: 29,
+                nombre: "M.",
+                apellido: "Carr",
+                imagenUsuario: "/images/users/imagenUsuario4.png"
+            },
+            {
+                id: 30,
+                nombre: "A.",
+                apellido: "Ruiz",
+                imagenUsuario: "/images/users/imagenUsuario0.png"
+            },
+            {
+                id: 31,
+                nombre: "I.",
+                apellido: "Stewart",
+                imagenUsuario: "/images/users/imagenUsuario1.png"
+            },
+            {
+                id: 32,
+                nombre: "F.",
+                apellido: "Taboada",
+                imagenUsuario: "/images/users/imagenUsuario2.png"
+            },
+            {
+                id: 33,
+                nombre: "S.",
+                apellido: "Ghilotti",
+                imagenUsuario: "/images/users/imagenUsuario3.png"
+            },
+            {
+                id: 34,
+                nombre: "J.",
+                apellido: "Strauss",
+                imagenUsuario: "/images/users/imagenUsuario4.png"
+            },
+            {
+                id: 35,
+                nombre: "P.",
+                apellido: "Kovacic",
+                imagenUsuario: "/images/users/imagenUsuario0.png"
+            },
+            {
+                id: 36,
+                nombre: "M.",
+                apellido: "Gomes",
+                imagenUsuario: "/images/users/imagenUsuario1.png"
+            },
+            {
+                id: 37,
+                nombre: "M.",
+                apellido: "Lopez",
+                imagenUsuario: "/images/users/imagenUsuario2.png"
+            },
+            {
+                id: 38,
+                nombre: "R.",
+                apellido: "Leiva",
+                imagenUsuario: "/images/users/imagenUsuario3.png"
+            },
+            {
+                id: 39,
+                nombre: "F.",
+                apellido: "Ortiz",
+                imagenUsuario: "/images/users/imagenUsuario4.png"
+            },
+
     ]
 }
 module.exports = users;
