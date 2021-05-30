cd34c09a9af88f744a516f0ee5ef35b45bbdd8c1
luisanazar <nazaranchorenal@udesa.edu.ar>
2021-05-30 12:57:04 -0300

    instalamos session express, app.js

--- ProyectoIntegrador2/routes/users.js
+++ ProyectoIntegrador2/routes/users.js
@@ -8,8 +8,12 @@ let usersController = require('../controllers/usersController');
 router.get ('/profileOthers/:id', usersController.profileOthers); //devuelve el perfil de los comentarios
 
 router.get ('/register', usersController.register); //devuelve el form para registrarse
+router.post ('/', usersController.store); //ponerlo en el controller
 
 router.get ('/login', usersController.login); //devuelve el form para inciar sesion
+router.post ('/login', usersController.processLogin); //ponerlo en el controller
+router.post ('logout', usersController.logout); //ponerlo en el controller
+
 
 router.get ('/profile', usersController.profile); //devuelve el perfil del usuario logeado
 
